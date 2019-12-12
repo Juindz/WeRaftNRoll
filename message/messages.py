2b63e87c1554b7a6a502fa2ff8e5e26890229b19
Juindz <47649121+Juindz@users.noreply.github.com>
2019-12-11 17:48:01 -0800

    Update messages.py

--- message/messages.py
+++ message/messages.py
@@ -11,11 +11,11 @@ class Message(object):
     RequestVoteResponse = 4
 
     # underscore before variable to define private variable
-    def __init__(self, sender, receiver, term, data):
+    # message sent by a sender (its port number )and to a reciever(its port number), has its own term
+    def __init__(self, sender, receiver, term):
         self._sender = sender
         self._receiver = receiver
         self._term = term
-        self._data = data
         time_record = time.time()
         self._timeStamp = int(time_record)
 
@@ -28,20 +28,12 @@ class Message(object):
         return self._sender
 
     @property
-    def data(self):
-        return self._data
-
-    @property
     def timestamp(self):
         return self._timeStamp
 
     @property
     def term(self):
         return self._term
-
-    @data.setter
-    def data(self, value):
-        self._data = value
 
     @term.setter
     def term(self, value):
@@ -58,33 +50,48 @@ class Message(object):
 
 class RequestVote(Message):
 
-    def __init__(self, sender, receiver, term, data):
-        Message.__init__(self, sender, receiver, term, data)
+    def __init__(self, sender, receiver, term, lastLogIndex, lastLogTerm):
+        Message.__init__(self, sender, receiver, term)
+        self._lastLogIndex = lastLogIndex
+        self._lastLogTerm = lastLogTerm
         self._type = Message.RequestVote
 
     @property
     def type(self):
         return self._type
 
+    @property
+    def lastLogTerm(self):
+        return self._lastLogTerm
+
+    @property
+    def lastLogIndex(self):
+        return self._lastLogIndex
 
 class RequestVoteResponse(Message):
 
-    def __init__(self, sender, receiver, term, data):
-        Message.__init__(self, sender, receiver, term, data)
+    def __init__(self, sender, receiver, term, voteGranted):
+        Message.__init__(self, sender, receiver, term)
         self._type = Message.RequestVoteResponse
+        self._voteGranted = voteGranted
 
     @property
     def type(self):
         return self._type
+    
+    @property
+    def voteGranted(self):
+        return self._voteGranted
+
 
 
 class AppendEntry(Message):
 
-    def __init__(self, sender, receiver, term, entries, commitIndex, prevLogIndex, prevLogTerm, data):
-        Message.__init__(self, sender, receiver, term, data)
+    def __init__(self, sender, receiver, term, prevLogIndex, prevLogTerm, entries, leaderCommit):
+        Message.__init__(self, sender, receiver, term)
         self._type = Message.AppendEntry
         self._entries = entries
-        self._commitIndex = commitIndex
+        self._leaderCommit = leaderCommit
         self._prevLogTerm = prevLogTerm
         self._prevLogIndex = prevLogIndex
 
@@ -97,8 +104,8 @@ class AppendEntry(Message):
         return self._entries
 
     @property
-    def commitIndex(self):
-        return self._commitIndex
+    def leaderCommit(self):
+        return self._leaderCommit
 
     @property
     def prevLogTerm(self):
@@ -110,12 +117,12 @@ class AppendEntry(Message):
 
 
 class AppendEntryResponse(Message):
-
-    def __init__(self, sender, receiver, term, success, matchIndex, data):
-        Message.__init__(self, sender, receiver, term, data)
+    # success = True if pass prevelogindex and prevlogterm checks
+    # term = current term
+    def __init__(self, sender, receiver, term, success):
+        Message.__init__(self, sender, receiver, term)
         self._success = success
         self._type = Message.AppendEntryResponse
-        self._matchIndex = matchIndex
 
     @property
     def type(self):
@@ -124,11 +131,6 @@ class AppendEntryResponse(Message):
     @property
     def success(self):
         return self._success
-
-
-    @property
-    def matchIndex(self):
-        return self._matchIndex
 
 
 
